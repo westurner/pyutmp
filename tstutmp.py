372b0a6a28690460eed6ce5e1d4b0f045b56d1c7
Wes Turner <wes@wrd.nu>
2019-11-20 23:50:17 -0500

    REF,TST: tstutmp.py: wrap with unittest.TestCase, add print_all example

--- tstutmp.py
+++ tstutmp.py
@@ -1,17 +1,32 @@
 from __future__ import print_function
-import pyutmp
+import pprint
+import subprocess
 import time
 import tty
-import subprocess
+import unittest
+
+import pyutmp
+
 
 def tty():
-    p = subprocess.Popen('tty', stdout=subprocess.PIPE)
+    p = subprocess.Popen("tty", stdout=subprocess.PIPE)
     p.wait()
     if p.returncode != 0:
-        raise Exception('tty(1) failed')
+        raise Exception("tty(1) failed")
     return p.stdout.readlines()[0].strip()
 
-f = UtmpFile()
-for u in f:
-    if u.ut_user_process:
-        print('%s %s (%s) from %s' % (time.ctime(u.ut_time), u.ut_user, u.ut_line, u.ut_host))
+
+class TestPyutmp(unittest.TestCase):
+    def test_ut_user_process(self):
+        f = pyutmp.UtmpFile()
+        for u in f:
+            if u.ut_user_process:
+                print(
+                    "%s %s (%s) from %s"
+                    % (time.ctime(u.ut_time), u.ut_user, u.ut_line, u.ut_host)
+                )
+
+    def test_print_all(self):
+        for f in pyutmp.UtmpFile():
+            assert f.__dict__
+            pprint.pprint(f.__dict__)
